7bc52f2775fbd945abe8307396481d374d781e1d
Peter Alserda <peteralserda@hotmail.com>
2017-08-11 00:17:27 +0200

    Initial commit

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,82 @@
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+  root: path.join(__dirname, '..'),
+  src: path.join(__dirname, '..', 'src'),
+  build: path.join(__dirname, '..', 'build'),
+};
+
+module.exports = {
+  context: paths.root,
+  devtool: 'cheap-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    'webpack/hot/dev-server',
+    path.join(paths.src, 'app.js'),
+  ],
+  output: {
+    path: paths.build,
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['.json', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+        },
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              outputStyle: 'expanded',
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?sourceMap',
+        ]
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(jpg|png|ttf|eot|svg)(\?v=.+)?$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        context: __dirname,
+        postcss: [autoprefixer]
+      }
+    }),
+    new HtmlWebpackPlugin({ template: 'public/index.html' }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+};
