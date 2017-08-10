4bd30e2468bacf6b992ee5569b742989f7802d5e
Peter Alserda <peteralserda@hotmail.com>
2017-08-11 00:33:43 +0200

    npm packages added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-https://hackernoon.com/my-journey-toward-a-maintainable-project-structure-for-react-redux-b05dfd999b5
+- https://hackernoon.com/my-journey-toward-a-maintainable-project-structure-for-react-redux-b05dfd999b5
+- https://medium.com/@alexmngn/how-to-better-organize-your-react-applications-2fd3ea1920f1
+
+- Redux-saga
+- Immutable js
+- Ducks
--- config/server.js
+++ config/server.js
@@ -1,1 +1,31 @@
-server.js
+const app = require('express')();
+const server = require('http').Server(app); // eslint-disable-line new-cap
+
+const port = 3000;
+
+/* eslint-disable import/no-extraneous-dependencies, import/newline-after-import */
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config.dev.js');
+const compiler = webpack(config);
+/* eslint-enable */
+
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  filename: config.output.filename,
+  publicPath: config.output.publicPath,
+  stats: {
+    colors: true,
+  },
+}));
+
+app.use(webpackHotMiddleware(compiler));
+
+server.listen(port, 'localhost', (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info(`==> 🌎  Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`);
+  }
+});
