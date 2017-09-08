c0ba3c682370555b3720bbaee334254b3e455ca5
Peter Alserda <peteralserda@hotmail.com>
2017-09-08 13:20:10 +0200

    Component detail filled in a bit

--- src/data/posts/selectors.js
+++ src/data/posts/selectors.js
@@ -1,8 +1,14 @@
 import { createSelector } from 'reselect';
 
 const getPostsMap = state => state.getIn(['posts', 'collection']);
+const getPostRecord = state => state.getIn(['posts', 'detail']);
 
 export const getPosts = createSelector(
   getPostsMap,
   (postsMap) => postsMap
+);
+
+export const getPost = createSelector(
+  getPostRecord,
+  (postRecord) => postRecord,
 );
--- src/scenes/Posts/scenes/Detail/index.js
+++ src/scenes/Posts/scenes/Detail/index.js
@@ -6,24 +6,39 @@ import { actions as postsActions } from 'data/posts/duck';
 import * as selectors from 'data/posts/selectors';
 
 
-class PostsNew extends Component {
+class PostsDetail extends Component {
   componentWillMount() {
     console.log('this.props: ', this.props);
     this.props.actions.fetchOne(this.props.match.params.id);
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('should component update', nextProps);
+    return true;
+    // return nextProps.post.get('id') === nextProps.match.params.id;
+  }
+
   render() {
+    const { post } = this.props;
+    console.log('posts detail props: ', this.props);
     return (
       <div>
-        postsnew
+        <h1>{post.get('title')}</h1>
+        <h5>Posted on: {post.get('createdAt')}</h5>
+        <h5>Updated on: {post.get('updatedAt')}</h5>
+        <p>{post.get('body')}</p>
       </div>
     );
   }
 }
 
+const mapStateToProps = state => ({
+  post: selectors.getPost(state)
+});
+
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators(postsActions, dispatch)
 });
 
-export default connect(null, mapDispatchToProps)(PostsNew);
+export default connect(mapStateToProps, mapDispatchToProps)(PostsDetail);
 
